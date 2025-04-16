73c921c42f9704bf5c71f95c5f635e76e8a667ca
zeqilin <zeqi.lin@alumnos.upm.es>
2025-04-16 19:12:58 +0200

    funcion dibujar tablero implementado falta ajustar el tamaño y la posicion del ojo

--- src/tablero.cpp
+++ src/tablero.cpp
@@ -1,5 +1,29 @@
 #include "tablero.h"
 
 void tablero::dibujar_tablero() {
+    // Dibuja el tablero 8x8
+    for (int i = 0; i < columnas; ++i) {
+        for (int j = 0; j < filas; ++j) {
+            // Calcula las coordenadas de la esquina superior izquierda de la casilla
+            float x = i * tamañocuadrado;
+            float y = j * tamañocuadrado;
 
+            // Si el la fila par columna par o fila impar columna impar es blanco de lo contrario negro
+            if ((i % 2 != 0 && j%2!=0)||(i%2==0&&j%2==0)) {
+                glColor4f(1.0f, 1.0f, 1.0f, 0.0f);
+            }
+            else{
+            
+                glColor3f(0.0f, 0.0f, 0.0f);
+            }
+
+            // va dibujando los cuadrados a medida que avanzan los bucles
+            glBegin(GL_QUADS);
+            glVertex2f(x, y);
+            glVertex2f(x + tamañocuadrado, y);
+            glVertex2f(x + tamañocuadrado, y + tamañocuadrado);
+            glVertex2f(x, y + tamañocuadrado);
+            glEnd();
+        }
+    }
 }
--- src/tablero.h
+++ src/tablero.h
@@ -1,7 +1,8 @@
 #pragma once
+#include "freeglut.h"
 class tablero
 {
-	int filas{ 8 }, columnas{ 8 };
+	int filas{ 8 }, columnas{ 8 },tamañocuadrado{1};
 public:
 	void dibujar_tablero();
 };
