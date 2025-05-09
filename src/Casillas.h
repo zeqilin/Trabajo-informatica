40114a82835ddd4358c58a02c9ecee609b76d1c3
zeqilin <zeqi.lin@alumnos.upm.es>
2025-05-09 19:12:50 +0200

    añadido la clase casillas para poder coordinar despues la posicion en el tablero con las coordenadas

---
+++ src/Casillas.h
@@ -0,0 +1,24 @@
+#pragma once
+class Casillas
+{
+    int fila;
+    int columna;
+    float x;
+    float y;
+
+public:
+    //constructor
+    Casillas(int f = 0, int c = 0, float x = 0, float y = 0)
+        : fila(f), columna(c), x(x), y(y) {}
+
+    // Getters
+    int getFila() const { return fila; }
+    int getColumna() const { return columna; }
+    float getX() const { return x; }
+    float getY() const { return y; }
+
+    // Setters
+    void setPosicionTablero(int f, int c) { fila = f; columna = c; }
+    void setCoordenadas(float x, float y) { x = x; y = y; }
+};
+
