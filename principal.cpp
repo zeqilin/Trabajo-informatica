6de00343ee845efd47b3f866eda0e0eb0debb7b1
raquelclementebastante <80747527+raquelclementebastante@users.noreply.github.com>
2025-05-14 12:55:18 +0200

    modificación de principal.cpp
    He llamado a la clase peón para poder visualizarla. Además, he cambiado el color del fondo a gris claro para poder visualizar bien las figuras negras

---
+++ principal.cpp
@@ -0,0 +1,93 @@
+#include "freeglut.h"
+#include "ETSIDI.h"
+#include "tablero.h"
+#include "Caballo.h"
+#include "Peon.h"
+//NO HACE FALTA LLAMARLAS EXPLICITAMENTE
+void OnDraw(void); //esta funcion sera llamada para dibujar
+void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
+void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla	
+
+Peon peon;
+
+int main(int argc, char* argv[])
+{
+	
+	//Inicializar el gestor de ventanas GLUT
+	//y crear la ventana
+	glutInit(&argc, argv);
+	glutInitWindowSize(800, 600);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("MiJuego");
+
+	glClearColor(0.8f, 0.8f, 0.8f, 1.0f); // pongo el fondo gris claro para poder visualizar bien todas las piezas
+
+	//habilitar luces y definir perspectiva
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(40.0, 800 / 600.0f, 0.1, 150);
+
+	//Registrar los callbacks
+	glutDisplayFunc(OnDraw);
+	glutTimerFunc(25, OnTimer, 0);//le decimos que dentro de 25ms llame 1 vez a la funcion OnTimer()
+	glutKeyboardFunc(OnKeyboardDown);
+
+	//POSIBLE INICIALIZACION
+
+	//pasarle el control a GLUT,que llamara a los callbacks
+	glutMainLoop();
+
+	return 0;
+}
+
+void OnDraw(void)
+{
+	tablero tablero;
+	Caballo caballo;
+	
+	//Borrado de la pantalla	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//Para definir el punto de vista
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	//funciones de dibujo
+	gluLookAt(4, 4, 12,  // posicion del ojo
+		4, 4, 0.0,      // hacia que punto mira  (0,0,0) 
+		0.0, 1.0, 0.0);
+	tablero.dibujar_tablero();
+	caballo.dibujar_caballo();
+	peon.dibujar_peon();
+	
+	
+
+	//no borrar esta linea ni poner nada despues
+	glutSwapBuffers();
+}
+void OnKeyboardDown(unsigned char key, int x_t, int y_t)
+{
+	//poner aqui el código de teclado
+
+
+	glutPostRedisplay();
+}
+
+void OnTimer(int value)
+{
+	//poner aqui el código de animacion
+	//posiciono el peon de tal forma que sea visible, para que no lo tape el caballo
+	Coordenadas pos;
+	pos.x = 0;
+	pos.y = 2;
+	peon.setPosicion(pos);
+
+		//no borrar estas lineas
+	glutTimerFunc(25, OnTimer, 0);
+	glutPostRedisplay();
+}
+
+
