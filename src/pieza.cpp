ee39a8db99f5b9ce0dbf3d8effdaa9931881d12c
zeqilin <47862400+zeqilin@users.noreply.github.com>
2025-06-19 13:43:20 +0200

    Delete src/pieza.cpp

--- src/pieza.cpp
+++
@@ -1,9 +0,0 @@
-#include "pieza.h"
-
-//Constructor de la clase pieza
-pieza::pieza(int x, int y, bool color) : x(x), y(y), color(color) {}
-
-//Métodos de acceso
-int pieza::getX() { return x; }
-int pieza::getY() { return y; }
-bool pieza::getColor() { return color; }
