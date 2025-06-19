63df8a30b43c11ce787481b5ab92755355f8fae8
zeqilin <zeqi.lin@alumnos.upm.es>
2025-06-19 14:09:48 +0200

    creacion de la clase pieza

---
+++ src/Pieza.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Casillas.h"
+
+enum class Color { Blanco, Negro };
+
+class Pieza {
+
+    Casillas posicion;
+    Color color;
+
+public:
+    Pieza(int fila, int columna, Color col) : posicion(fila, columna), color(col) {}
+
+
+    virtual void dibujar() = 0;
+
+    Casillas getPosicion() const { return posicion; }
+    Color getColor() const { return color; }
+
+    void setPosicion(int fila, int columna, float x, float y) {
+        posicion.setPosicionTablero(fila, columna);
+        posicion.setCoordenadas(x, y);
+    }
+};
