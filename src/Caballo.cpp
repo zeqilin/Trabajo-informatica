3c07c1cc76b4c7fc85b5b3d7a7f5df7eff221a37
raquelclementebastante <raquel.clementeb@alumnos.upm.es>
2025-06-21 13:43:08 +0200

    pequeña modificación

--- src/Caballo.cpp
+++ src/Caballo.cpp
@@ -14,8 +14,8 @@ bool Caballo::movimientoValido(int filaDestino, int columnaDestino) {
     int filaActual = posicion.getFila();
     int colActual = posicion.getColumna();
 
-    int nf = std::abs(filaDestino - posicion.getFila()); //nf es el numero de filas que se movería el caballo
-    int nc = std::abs(columnaDestino - posicion.getColumna());//nf es el numero de columnas que se movería el caballo
+    int nf = std::abs(filaDestino - filaActual); //nf es el numero de filas que se movería el caballo
+    int nc = std::abs(columnaDestino - colActual);//nf es el numero de columnas que se movería el caballo
 
     return (nf == 2 && nc == 1) || (nf == 1 && nc == 2);//movimiento en L
 }
